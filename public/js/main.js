336c63e84b293f049c47ba3df20bcc8fe95d8c75
Kyle Gray <kylemaxgray@gmail.com>
2022-01-02 22:22:36 -0800

    add messaged for connect disconnect events

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,20 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
+
+socket.on('message', message => (
+  console.log(message);
+});
+
+// Message submit
+chatForm.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  // Get message text
+  const msg = event.target.elemts.msg.value;
+
+  // Emit message to server 
+  socket.emit('chatMessage', msg);
+});
+
 
