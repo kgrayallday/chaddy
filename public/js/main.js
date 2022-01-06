06ec6e96c0bb3775cb0934d1666bf2e25889fa27
Kyle Gray <kylemaxgray@gmail.com>
2022-01-05 22:34:27 -0800

    add output message to DOM logic

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,9 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io();
 
-socket.on('message', message => (
+socket.on('message', message => {
   console.log(message);
+  outputMessage(message);
 });
 
 // Message submit
@@ -11,10 +12,18 @@ chatForm.addEventListener('submit', event => {
   event.preventDefault();
   
   // Get message text
-  const msg = event.target.elemts.msg.value;
+  const msg = event.target.elements.msg.value;
 
   // Emit message to server 
   socket.emit('chatMessage', msg);
 });
 
-
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.innerHTML = '<p class="meta">Kyle <span>3:33pm</span></p>
+    <p class="text">
+      ${message}
+    </p>';
+  document.querySelector('.chat-messages').appendChild(div);
+}
